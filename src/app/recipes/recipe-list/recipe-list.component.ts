4da2ff562b3ccd78b5de33adc825d860d7c702cf
Moath <moathahmad320@gmail.com>
2023-06-12 19:57:33 -0400

    Recipe Detail &  List Framework

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('test recipe','sample description', `https://www.metmuseum.org/-/media/images/visit/
+    food-drink/the-eatery-ground-floor.jpg?h=960&w=1440&sc_lang=en&hash=3112A4CF79610CDD18D24E997DB975A6`),    
+    
+    new Recipe('test recipe','sample description', `https://www.metmuseum.org/-/media/images/visit/
+    food-drink/the-eatery-ground-floor.jpg?h=960&w=1440&sc_lang=en&hash=3112A4CF79610CDD18D24E997DB975A6`)
+  ];
 
 }
