da1d85c84bda570098318dee5cc102b555773dcb
Moath <moathahmad320@gmail.com>
2023-06-14 07:06:17 -0400

    Selecting Recipe from list Pulls details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,12 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('test recipe','sample description', `https://www.metmuseum.org/-/media/images/visit/
     food-drink/the-eatery-ground-floor.jpg?h=960&w=1440&sc_lang=en&hash=3112A4CF79610CDD18D24E997DB975A6`),    
     
-    new Recipe('test recipe','sample description', `https://www.metmuseum.org/-/media/images/visit/
+    new Recipe('test recipe 2','sample description', `https://www.metmuseum.org/-/media/images/visit/
     food-drink/the-eatery-ground-floor.jpg?h=960&w=1440&sc_lang=en&hash=3112A4CF79610CDD18D24E997DB975A6`)
   ];
+
+  onRecipeSelected(recipe: Recipe){
+this.recipeWasSelected.emit(recipe)
+  }
 
 }
