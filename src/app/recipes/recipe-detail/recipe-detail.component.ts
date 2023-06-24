58420887a464554d3549afa04bf9c5384f6eda09
Moath <moathahmad320@gmail.com>
2023-06-24 07:43:04 -0400

    Added Reactive Forms to Recipe List & Fully Functioning Recipe Edit Buttons

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -34,4 +34,9 @@ id: number;
   this.router.navigate(['edit'], {relativeTo: this.route});
   }
 
+  onDeleteRecipe(){
+    this.recipeService.deleteRecipe(this.id);
+    this.router.navigate(['/recipes']);
+  }
+
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,12 @@
 import{Injectable} from '@angular/core'
+import {Subject} from 'rxjs';
 import { Recipe } from "./recipe.model";
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService{
+  recipesChanged = new Subject<Recipe[]>();
 
     private recipes: Recipe[] = [
         new Recipe('test recipe','sample description', `https://www.metmuseum.org/-/media/images/visit/
@@ -34,4 +36,18 @@ export class RecipeService{
         this.shoppingService.addIngredients(ingredients);
       }
 
+      addRecipe(recipe: Recipe){
+        this.recipes.push(recipe);
+        this.recipesChanged.next(this.recipes.slice());
+      }
+
+      updateRecipe(index: number, newRecipe: Recipe){
+        this.recipes[index] = newRecipe;
+        this.recipesChanged.next(this.recipes.slice());
+      }
+
+      deleteRecipe(index: number){
+        this.recipes.splice(index, 1);
+        this.recipesChanged.next(this.recipes.slice());
+      }
 }
